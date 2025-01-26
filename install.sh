49b3f48cb58360d6e85e2919a088ec85536c20fd
nxjii764 <nxjii864@gmail.com>
2025-01-27 02:06:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/sahil/sahil.py",
+    "postStartCommand": "python3 /workspaces/863335/sahil.py",
     "customizations": {
         "vscode": {
             "settings": {
